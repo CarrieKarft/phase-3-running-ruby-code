baccc59d451e508141a87154882353c5b03a8507
Carrie Kraft <carriekarft@gmail.com>
2023-04-20 17:12:56 -0700

    commit tree e642c07e641c1fe4337045684eca2d4d3be845db

--- app.rb
+++ app.rb
@@ -1,23 +1,8 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
-
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
-
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
-
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
+# app.rb
+# print "Second Hello World"
+# print "hello" 
+# print "bye"
+# puts [1, 2, 3].inspect
+puts "Hello World!"
+print "Pass this test, please." 
+p [1, 2, 3]
